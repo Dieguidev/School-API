28e4718ce185968789564e997ecdb25254c52522
Dieguidev <diegogaraycullas@gmail.com>
2023-04-07 21:01:04 -0500

    agregando despliegue

--- src/app.js
+++ src/app.js
@@ -1,10 +1,16 @@
 const express = require('express');
+const cors = require('cors');
 const routerApi = require('./routes');
 const { logErrors, errorHandles, boomErrorHandler } = require('./middlewares/error.handler');
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
+
+app.get('/',(req, res) => {
+  res.send('Hola mi server en express');
+});
 
 routerApi(app)
 
